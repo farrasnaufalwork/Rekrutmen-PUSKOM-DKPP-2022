bacf95c9e722327370be28ac4d6a55361067c87d
farrasnaufalwork <farrasnaufalwork@gmail.com>
2022-04-16 17:06:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classNames from "classnames";
 
-function App() {
+export default function App() {
+  const [likesCounter, setLikesCounter] = React.useState(100);
+  const [likesButton, setLikesButton] = React.useState(false);
+  const [exe, setExe] = React.useState('')
+
+  const doLike = () => {
+    setLikesButton(!likesButton);
+    setLikesCounter(likesButton ? 100 : 101);
+  };
+
+  let btnLikeClass = classNames("like-button", "like-button");
+  let btnLikeCounter = classNames("likes-counter", "likes-counter");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ padding: 16 }}>
+      <button
+        onClick={() => doLike()}
+        className={btnLikeClass}
+        style={{ width: 150, flexDirection: "row", display: "flex" }}
+      >
+        {/* <div
+        style={{
+          display: 'flex',
+          flexDirection: "row",
+          alignItems : 'center'
+        }}
         >
-          Learn React
-        </a>
-      </header>
+
+        </div> */}
+        <img src={likesButton ? '/image/like-blue.svg' :  '/image/like-black.svg'} style={{ width: "20px", height: "20px", position: 'relative', top: '9px', left: '5px' }} />
+        <p
+          style={{
+            display: "flex",
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            color: likesButton ? "blue" : "black",
+          }}
+        >
+          {likesButton ? "Liked" : "Like"}
+        </p>
+        <p style={{ display: "flex", color: likesButton ? "blue" : "black" }}>
+          |
+        </p>
+        <p
+          className={btnLikeCounter}
+          style={{
+            display: "flex",
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            color: likesButton ? "blue" : "black",
+          }}
+        >
+          {likesCounter}
+        </p>
+      </button>
+
+
     </div>
   );
 }
-
-export default App;
